e7c2e6097a80a94b34d8890e8c3c3755df6639b7
Miasma24 <reo050224@gmail.com>
2024-09-24 08:47:18 +0900

    クオリティ上げ1

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,9 @@ function App() {
   ];
   const [selectedImage, setSelectedImage] = useState<number | null>(null);
   const [size, setSize] = useState<number>(150);
+  const [alpha, setAlpha] = useState<number>(1.0);
+  const [atarihazure, tyuusen] = useState<boolean | null>(null);
+  const [isAnimEnd, setAnimState] = useState<boolean>(false);
 
   useEffect(() => {
     liff
@@ -36,44 +39,98 @@ function App() {
 
   const handleClick = (index: number) => {
     setSelectedImage(index);
+    tyuusen(Math.random() * 3 < 1.0 ? true : false);
 
     // 拡大アニメーション
     const interval = setInterval(() => {
       setSize((prevSize) => {
         if (prevSize >= 400) {
           clearInterval(interval);
+          setAnimState(true);
           return prevSize;
         }
         return prevSize + 10;
+      });
+      setAlpha((prevAlpha) => {
+        if (prevAlpha <= 0) {
+          clearInterval(interval);
+          setAnimState(true);
+          return prevAlpha;
+        }
+        return prevAlpha - 0.04;
       });
     }, 100);
   };
 
   return (
-    <div className="image-container">
-      {selectedImage === null ? (
-        images.map((src, index) => (
-          <img
-            key={index}
-            src={TreasureChestClosed}
-            alt="../images/sozai_image_103600.png"
-            className="image"
-            onClick={() => handleClick(index)}
-            style={{
-              cursor: "pointer",
-              width: 100,
-              height: 100
-            }}
-          />
-        ))
-      ) : (
-        <img
-          src={TreasureChestOpened}
-          alt="../images/sozai_image_103598.png"
-          className="image"
-          style={{ width: `${size}px`, height: `${size}px` }}
-        />
-      )}
+    <div>
+      <div className="BG">
+        <div className="atarihazureText">
+          {selectedImage === null ?
+            <center>
+              <p>
+                宝箱を一つ選んでタップしてね！
+              </p>
+            </center>
+            :
+            <p>
+            </p>
+          }
+          <div className="image-container">
+            {selectedImage === null ? (
+              images.map((src, index) => (
+                <div className={index === 0 ? "image" : index === 1 ? "image2" : "image3"}>
+                  <img
+                    key={index}
+                    src={TreasureChestClosed}
+                    alt="閉じた宝箱"
+                    //className={index === 0 ? "image" : index === 1 ? "image2" : "image3"}
+                    onClick={() => handleClick(index)}
+                    style={{
+                      cursor: "pointer",
+                      width: 100,
+                      height: 100
+                    }}
+                  />
+                </div>
+              ))
+            ) : isAnimEnd ? (
+              <div className="atarihazureText">
+                <center>
+                  {atarihazure ?
+                    <strong>
+                      <p>
+                        あたり！<br />
+                        クーポン獲得！
+                      </p>
+                    </strong>
+                    :
+                    <strong>
+                      <p>
+                        はずれ！<br />
+                        また明日挑戦してね！
+                      </p>
+                    </strong>
+                  }
+                  <button
+                    onClick={() => window.close()}>
+                    戻る
+                  </button>
+                </center>
+              </div>
+            ) : (
+              <div className={selectedImage === 0 ? "image" : selectedImage === 1 ? "image2" : "image3"}>
+                <img
+                  src={TreasureChestOpened}
+                  alt="開いた宝箱"
+                  className="image"
+                  style={{ width: `${size}px`, height: `${size}px`, opacity: `${alpha}` }}
+                />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
